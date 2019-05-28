43f8f554aad6e3b17d3183ad2bad2ce4e6ffad37
snrk622 <s.0622.r@icloud.com>
2019-05-28 21:52:12 +0900

    レスポンシブに対応

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 (function () {
     'use strict';
-
+    
+    // let targetElement = document.getElementById("skills");
     let vm = new Vue({
-        el: '#window',
+        el: '#wrapper',
         data: {
-            activeTab: 1
+            activeTab: 1,
+            targetRect: 0,
+            animation: false
+        },
+        mounted() {
+            this.position();
+            window.addEventListener('scroll', this.position);
         },
         methods: {
             clickTabA: function () {
@@ -12,7 +19,22 @@
             },
             clickTabB: function () {
                 this.activeTab = 2;
+            },
+            position: function () {
+                // ターゲットを取得
+                let target = document.getElementById("programing_skills");
+                // ターゲットの座標を取得
+                let rect = target.getBoundingClientRect().top;
+                // dataのtargetRectを更新
+                this.targetRect = rect;
+                //ウィンドウの高さ
+                let windowHeight = window.innerHeight;
+                if (this.targetRect <= (windowHeight / 2)) {
+                    this.animation = true;
+                    window.removeEventListener('scroll', this.position);
+                }
             }
+
         }
     });
 
