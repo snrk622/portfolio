6f7b21e68960db7978be56d5dc5308ed87c1b2f1
snrk622 <s.0622.r@icloud.com>
2019-05-30 23:49:58 +0900

    自己紹介追加

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 (function () {
     'use strict';
-    
+
     // let targetElement = document.getElementById("skills");
     let vm = new Vue({
         el: '#wrapper',
         data: {
             activeTab: 1,
             targetRect: 0,
-            animation: false
+            animation: false,
+            background: 'linear-gradient(bottom, #00a381 0%, #00a381 40%, #eee 41%, #eee 100%)',
+            pageTopBtn: document.getElementById('pagetop')
         },
         mounted() {
             this.position();
+            this.scrollWindow();
+            this.scrollTop();
             window.addEventListener('scroll', this.position);
+            window.addEventListener('scroll', this.scrollWindow);
         },
         methods: {
             clickTabA: function () {
@@ -33,6 +38,29 @@
                     this.animation = true;
                     window.removeEventListener('scroll', this.position);
                 }
+            },
+            scrollWindow: function () {
+                let scroll = window.pageYOffset;
+                let windowHeight = window.innerHeight;
+                let pageHeight = document.documentElement.scrollHeight;
+                this.background = (scroll / (pageHeight - windowHeight)) * 100 + '%';
+            },
+            scrollTop: function () {
+                let target = document.getElementById('pagetop');
+                target.addEventListener('click', function () {
+                    let currentY = window.pageYOffset;
+                    let step = 500 / currentY > 1 ? 10 : 100;
+                    let timeStep = 500 / currentY * step;
+                    let intervalID = setInterval(scrollUp, timeStep);
+                    function scrollUp() {
+                        currentY = window.pageYOffset;
+                        if (currentY === 0) {
+                            clearInterval(intervalID);
+                        } else {
+                            scrollBy(0, -step);
+                        }
+                    }
+                });
             }
 
         }
